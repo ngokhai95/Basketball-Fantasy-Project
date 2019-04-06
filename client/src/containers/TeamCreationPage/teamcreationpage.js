8e3062797be8be9d10305ac43db4aac8c32fbd62
YummyBlaBla <gorick2491@hotmail.com>
2019-04-05 21:22:20 -0700

    Finish front end portion of team creation page

--- client/src/containers/TeamCreationPage/teamcreationpage.js
+++ client/src/containers/TeamCreationPage/teamcreationpage.js
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import axios from "axios";
+import Badge from "react-bootstrap/Badge";
+import Modal from "react-bootstrap/Modal";
 
 const SERVER_ADDRESS = "http://127.0.0.1:8000";
 
 class TeamCreationPage extends Component {
 	constructor(props) {
 		super(props);
+
+		this.handleShow = this.handleShow.bind(this);
+		this.handleClose = this.handleClose.bind(this);
 
 		let playerList = this.props.teamCreation.slice(
 			0,
@@ -28,9 +33,24 @@ class TeamCreationPage extends Component {
 		}
 
 		this.state = {
-			players: this.props.teamCreation
+			players: this.props.teamCreation,
+			selectedPlayer: null,
+			show: false
 		};
 	}
+
+	handleClose = () => {
+		this.setState({ show: false });
+	};
+
+	handleShow = () => {
+		this.setState({ show: true });
+	};
+
+	handleToggleSellPlayer = player => {
+		this.setState({ selectedPlayer: player });
+		this.handleShow();
+	};
 
 	goToSearchPage = index => {
 		this.props.history.push({
@@ -38,9 +58,26 @@ class TeamCreationPage extends Component {
 		});
 	};
 
-	render() {
-		let teamInfo = this.props.teamInfo;
+	// TODO: send server to sell
+	confirmSell = () => {
+		console.log("do something");
+	};
 
+	render() {
+		let sellConfirm = null;
+
+		if (this.state.show) {
+			sellConfirm = (
+				<SellConfirm
+					handleClose={this.handleClose}
+					show={this.state.show}
+					selectedPlayer={this.state.selectedPlayer}
+					confirmAdd={this.handleToggleSellPlayer}
+				/>
+			);
+		}
+
+		let teamInfo = this.props.teamInfo;
 		let teamCreation = this.state.players;
 
 		const teamMembers = teamCreation.map((player, index) => {
@@ -56,7 +93,21 @@ class TeamCreationPage extends Component {
 					</Button>
 				);
 			} else {
-				return <p key={index}>player: {player.name}</p>;
+				return (
+					<h1 key={index}>
+						<Badge pill variant="secondary">
+							{player.name}
+						</Badge>
+						<Button
+							variant="warning"
+							onClick={() => {
+								this.handleToggleSellPlayer(player);
+							}}
+						>
+							Sell
+						</Button>
+					</h1>
+				);
 			}
 		});
 		return (
@@ -64,9 +115,38 @@ class TeamCreationPage extends Component {
 				<h1>Team Creation Page</h1>
 				<p>Team Name: {teamInfo.team_name}</p>
 				<ButtonGroup vertical>{teamMembers}</ButtonGroup>
+				{sellConfirm}
 			</div>
 		);
 	}
 }
+
+const SellConfirm = props => {
+	return (
+		<Modal show={props.show} onHide={props.handleClose}>
+			<Modal.Header closeButton>
+				<Modal.Title>Sell Player</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				Are you sure you want to sell {props.selectedPlayer.name} on
+				your team?
+			</Modal.Body>
+			<Modal.Footer>
+				<Button variant="secondary" onClick={props.handleClose}>
+					No
+				</Button>
+				<Button
+					variant="primary"
+					onClick={() => {
+						props.confirmSell(props.selectedPlayer);
+						props.handleClose();
+					}}
+				>
+					Yes, Sell!
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	);
+};
 
 export default TeamCreationPage;
